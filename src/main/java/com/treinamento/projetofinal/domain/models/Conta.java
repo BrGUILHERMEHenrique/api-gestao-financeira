211bf55e55224d18b0e002ca8f20dddfff966f82
BrGUILHERMEHenrique <guilhermehenriquersouza17@gmail.com>
2021-01-07 17:57:40 -0300

    Criando classe de conta

---
+++ src/main/java/com/treinamento/projetofinal/domain/models/Conta.java
@@ -0,0 +1,134 @@
+package com.treinamento.projetofinal.domain.models;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CascadeType;
+
+import com.sun.istack.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="conta")
+public class Conta implements Comparable<Conta>{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@OneToOne
+	@JoinColumn(name = "id_usuario")
+	@Cascade(value = {CascadeType.ALL})
+	private Usuario usuario;
+	
+	@NotNull
+	private String descricao;
+	
+	@NotNull
+	private String tipoGasto;
+	
+	@NotNull
+	private Double preco;
+	
+	@NotNull
+	private Date dataVencimento;
+	
+	@NotNull
+	private Boolean paga = false;
+
+	public Conta() {
+		super();
+	}
+
+	public Conta(Long id, Usuario usuario, String descricao, String tipoGasto, Double preco, Date dataVencimento,
+			Boolean paga) {
+		super();
+		this.id = id;
+		this.usuario = usuario;
+		this.descricao = descricao;
+		this.tipoGasto = tipoGasto;
+		this.preco = preco;
+		this.dataVencimento = dataVencimento;
+		this.paga = paga;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getTipoGasto() {
+		return tipoGasto;
+	}
+
+	public void setTipoGasto(String tipoGasto) {
+		this.tipoGasto = tipoGasto;
+	}
+
+	public Double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	public Boolean getPaga() {
+		return paga;
+	}
+
+	public void setPaga(Boolean paga) {
+		this.paga = paga;
+	}
+
+	@Override
+	public int compareTo(Conta o) {
+		return this.dataVencimento.compareTo(o.getDataVencimento());
+	}
+	
+	
+	
+}
