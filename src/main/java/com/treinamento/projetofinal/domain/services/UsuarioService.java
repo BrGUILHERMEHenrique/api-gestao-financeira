9c19dfe8a63db8c3095e05b6ffb22e49b157161a
BrGUILHERMEHenrique <guilhermehenriquersouza17@gmail.com>
2021-01-07 17:48:23 -0300

    Criando lógica de para entidade usuario

--- src/main/java/com/treinamento/projetofinal/domain/services/UsuarioService.java
+++ src/main/java/com/treinamento/projetofinal/domain/services/UsuarioService.java
@@ -1,18 +1,23 @@
 package com.treinamento.projetofinal.domain.services;
 
-import java.util.List;
+import java.util.Optional;
 
 import javax.transaction.Transactional;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.treinamento.projetofinal.domain.models.Conta;
+import com.treinamento.projetofinal.domain.dtos.UsuarioDto;
 import com.treinamento.projetofinal.domain.models.Usuario;
+import com.treinamento.projetofinal.domain.models.exceptions.UsuarioNaoEncontradoException;
 import com.treinamento.projetofinal.infrastructure.repositories.UsuarioRepository;
 
 @Service
 public class UsuarioService {
+	
+	@Autowired
+	ModelMapper modelMapper;
 
 	@Autowired
 	UsuarioRepository usuarioRepository;
@@ -20,25 +25,35 @@ public class UsuarioService {
 	@Autowired
 	ContaService contaService;
 	
-	@Transactional
-	public Usuario retornaUsuario(Long id) {
-		return usuarioRepository.findById(id).get();
+	private Usuario dtoToModel(UsuarioDto dto) {
+		return modelMapper.map(dto, Usuario.class);
 	}
 	
-	@Transactional 
-	public Usuario criarUsuario(Usuario usuario) {
+	@Transactional
+	public Usuario retornaUsuario(Long id) throws UsuarioNaoEncontradoException {
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
+		if(usuario.isPresent()) {
+			return usuario.get();
+		} else {
+			throw new UsuarioNaoEncontradoException();
+		}
+	}
+	
+	@Transactional
+	public Usuario criarUsuario(UsuarioDto dto) {
+		Usuario usuario = dtoToModel(dto);
 		return usuarioRepository.save(usuario);
 	}
 	
 	@Transactional
-	public Usuario atualizaUmUsuario(Long id, Usuario usuario) {
+	public Usuario atualizaUmUsuario(Long id, Usuario usuario) throws UsuarioNaoEncontradoException {
 		Usuario usuarioAchado = retornaUsuario(id);
 		
 		return usuarioRepository.save(usuario.verificaDadosNulos(usuarioAchado));
 	}
 	
 	@Transactional
-	public String adcionarSaldo(Long id, Double saldo) {
+	public String adcionarSaldo(Long id, Double saldo) throws UsuarioNaoEncontradoException {
 		Usuario usuario = retornaUsuario(id);
 		Double saldoTotal = usuario.getSaldo() + saldo;
 		usuario.setSaldo(saldoTotal);
@@ -47,7 +62,7 @@ public class UsuarioService {
 	}
 	
 	@Transactional
-	public String retirarSaldo(Long id, Double quantia) {
+	public String retirarSaldo(Long id, Double quantia) throws UsuarioNaoEncontradoException {
 		Usuario usuario = retornaUsuario(id);
 		Double saldoRestante = usuario.getSaldo() - quantia;
 		usuario.setSaldo(saldoRestante);
@@ -55,8 +70,12 @@ public class UsuarioService {
 		return "Quantia retirada com sucesso";
 	}
 	
-	public List<Conta> retornaListaContasUsaurio(Long id) {
-		return contaService.retornaContasUsuario(id);
+	@Transactional
+	public String apagarUsuario(Long id) {
+		usuarioRepository.deleteById(id);
+		
+		return "Usuário apagado com sucesso";
 	}
+	
 	
 }
