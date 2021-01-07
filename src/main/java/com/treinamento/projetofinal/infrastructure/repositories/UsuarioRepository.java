b672e85d0343180a858c814abffe618b9128dd8b
BrGUILHERMEHenrique <guilhermehenriquersouza17@gmail.com>
2021-01-07 17:49:10 -0300

    Criando repositório para comunicação com o banco

--- src/main/java/com/treinamento/projetofinal/infrastructure/repositories/UsuarioRepository.java
+++ src/main/java/com/treinamento/projetofinal/infrastructure/repositories/UsuarioRepository.java
@@ -1,9 +1,11 @@
 package com.treinamento.projetofinal.infrastructure.repositories;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.treinamento.projetofinal.domain.models.Usuario;
 
+@Repository
 public interface UsuarioRepository extends CrudRepository<Usuario, Long>{
 
 	Usuario findByNome(String nome);
