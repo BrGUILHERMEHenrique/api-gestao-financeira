d52642cda2a74655756badce9d3df1739692966f
BrGUILHERMEHenrique <guilhermehenriquersouza17@gmail.com>
2021-01-17 13:02:02 -0300

    Corrigindo erro lombok servidor

--- src/main/java/com/treinamento/projetofinal/application/dto/ContaDto.java
+++ src/main/java/com/treinamento/projetofinal/application/dto/ContaDto.java
@@ -2,17 +2,7 @@ package com.treinamento.projetofinal.application.dto;
 
 import java.util.Date;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Data
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class ContaDto {
 
 	private String descricao;
